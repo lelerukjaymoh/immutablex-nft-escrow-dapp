444ab4da8c23d671e47812049bdded66b9af1f2d
Jay <lelerukjaymoh@gmail.com>
2024-01-31 10:00:44 +0300

    feat: implement NFT minting
    - Allow users to mint NFTs throuhg their passport wallets without paying for fees

--- components/NFT.tsx
+++ components/NFT.tsx
@@ -7,6 +7,8 @@ import { blockchainData, config } from "@imtbl/sdk";
 import { displayPartialAddress } from "@/lib/utils";
 import SwapNFT from "./Swap";
 import Proposals from "./Proposals";
+import axios from "axios";
+import { Contract } from "ethers";
 
 /**
  * NFT Display.
@@ -15,7 +17,7 @@ export default function NFT() {
   const [userNFTs, setUserNFTs] = useState<any[]>([]);
   const [proposals, setProposals] = useState<any[]>([]);
   const [availableNFTs, setAvailableNFTs] = useState<any[]>([]);
-  const { accountAddress } = useSharedContext();
+  const { accountAddress, evmSigner, evmProvider } = useSharedContext();
 
   useEffect(() => {
     const client = new blockchainData.BlockchainData({
@@ -26,14 +28,14 @@ export default function NFT() {
     });
 
     const getNFTs = async () => {
+      console.log("Getting nfts ", accountAddress);
       try {
         // Get NFTs owned by logged in user
         const chainName = "imtbl-zkevm-testnet";
-        const contractAddress = "0x939660e5a9dd3efcc1ec0357b365b7625dd6f522";
         const response = await client.listNFTsByAccountAddress({
           chainName,
-          accountAddress: "0x7937ce7F2e6e316E91814055Bc430c4f5Cc226C6",
-          contractAddress,
+          accountAddress,
+          contractAddress: process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS!,
         });
 
         const _userNFTs = response.result;
@@ -42,7 +44,7 @@ export default function NFT() {
         // Get all NFTs available for swap
         const allNFTsResponse = await client.listNFTs({
           chainName,
-          contractAddress,
+          contractAddress: process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS!,
         });
 
         const allNFTs = allNFTsResponse.result;
@@ -67,12 +69,134 @@ export default function NFT() {
     };
 
     getNFTs();
-  }, []);
+  }, [accountAddress]);
+
+  const mint = async () => {
+    try {
+      const image = await getImage();
+
+      console.log("returned image ", image);
+
+      if (image) {
+        const contract = new Contract(
+          process.env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS!,
+          [
+            "function mint(address to, uint256 tokenId) public payable",
+            "function totalSupply() public view returns (uint256)",
+            "function grantMinterRole(address account) public",
+            "function totalSupply() public view returns (uint256)",
+          ],
+          evmSigner
+        );
+
+        const totalTokens = await contract.totalSupply(evmProvider as any);
+        const tokenId = parseInt(totalTokens) + 1;
+
+        console.log("new token id ", tokenId);
+
+        const txnData = await contract.mint.populateTransaction(
+          accountAddress,
+          tokenId
+        );
+
+        const transaction = await evmSigner?.sendTransaction(txnData);
+
+        console.log("transaction submitted ", transaction);
+
+        await transaction?.wait().then(async (receipt: any) => {
+          console.log("receipt ", receipt);
+          await refreshNFTMetadata(image, tokenId);
+        });
+      }
+    } catch (error) {
+      console.log("Error minting NFT ", error);
+    }
+  };
+
+  const refreshNFTMetadata = async (image: any, tokenId: number) => {
+    const options = {
+      method: "POST",
+      url: `https://api.sandbox.immutable.com/v1/chains/imtbl-zkevm-testnet/collections/${process
+        .env.NEXT_PUBLIC_NFT_CONTRACT_ADDRESS!}/nfts/refresh-metadata`,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "x-immutable-api-key": process.env.NEXT_PUBLIC_IMMUTABLE_API_KEY!,
+      },
+      data: {
+        nft_metadata: [
+          {
+            name: image.name,
+            description: image.description,
+            image: image.imageLink,
+            external_url: image.imageLink,
+            animation_url: image.imageLink,
+            youtube_url: image.imageLink,
+            attributes: [],
+            token_id: tokenId.toString(),
+          },
+        ],
+      },
+    };
+
+    try {
+      const { data } = await axios.request(options);
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const getImage = async () => {
+    try {
+      const { data } = await axios.get(
+        "https://api.unsplash.com/photos/random",
+        {
+          headers: {
+            Authorization: `Client-ID ${process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_KEY}`,
+          },
+        }
+      );
+
+      console.log("response ", data);
+
+      const description = data.description;
+      const altDescription = data.alt_description
+        ? data.alt_description
+        : "Data unavailable";
+
+      const imageName = description
+        ? description
+        : altDescription.split(" ").slice(0, 2).join(" ");
+
+      const image = {
+        name: imageName,
+        description: altDescription,
+        imageLink: data.urls.full,
+      };
+
+      console.log("image ", image);
+
+      return image;
+    } catch (error) {
+      console.error("Error fetching image:", error);
+    }
+
+    return null;
+  };
 
   return (
     <div style={{ top: "10px" }}>
       <div style={{ marginTop: "100px" }}>
-        <h2>User NFTs</h2>
+        <div className={styles.grid}>
+          <h2>User NFTs</h2>
+          <button
+            style={{ width: "fit-content", padding: "10px" }}
+            onClick={mint}
+          >
+            Mint NFT
+          </button>
+        </div>
         <div className={styles.description}>
           <div className={styles.grid}>
             {userNFTs.map((nft) => {
@@ -86,7 +210,66 @@ export default function NFT() {
 
                   <a href={nft.image} target="_blank" rel="noopener noreferrer">
                     {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img src={nft.image} alt={nft.description} />
+                    <img
+                      src={nft.image}
+                      alt={nft.description}
+                      width={200}
+                      height={200}
+                    />
+                  </a>
+                  <div style={{ marginTop: "20px", width: "100%" }}>
+                    <span style={{ paddingBottom: "10px" }}>
+                      {nft.description}
+                    </span>
+                    <span
+                      onClick={() => {
+                        navigator.clipboard.writeText(nft.contract_address);
+                        console.log(
+                          "copied contract address ",
+                          nft.contract_address
+                        );
+                      }}
+                      style={{ paddingBottom: "10px", cursor: "pointer" }}
+                    >
+                      Contract: {displayPartialAddress(nft.contract_address)}
+                    </span>
+                    <span
+                      onClick={() => {
+                        navigator.clipboard.writeText(nft.token_id);
+                        console.log("copied token id ", nft.token_id);
+                      }}
+                      style={{ cursor: "pointer" }}
+                    >
+                      Token ID: {nft.token_id}
+                    </span>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+      <div style={{ marginTop: "100px" }}>
+        <h2>NFTs available for swap</h2>
+        <div className={styles.description}>
+          <div className={styles.grid}>
+            {availableNFTs.map((nft) => {
+              return (
+                <div
+                  key={nft.token_id}
+                  className={styles.card}
+                  style={{ marginTop: "20px" }}
+                >
+                  <h2 style={{ textAlign: "center" }}>{nft.name}</h2>
+
+                  <a href={nft.image} target="_blank" rel="noopener noreferrer">
+                    {/* eslint-disable-next-line @next/next/no-img-element */}
+                    <img
+                      src={nft.image}
+                      alt={nft.description}
+                      width={200}
+                      height={200}
+                    />
                   </a>
                   <div style={{ marginTop: "20px", width: "100%" }}>
                     <span style={{ paddingBottom: "10px" }}>
@@ -115,58 +298,9 @@ export default function NFT() {
                     </span>
                   </div>
                   <SwapNFT
-                    proposerContractAddress={nft.contract_address}
-                    proposerTokenId={nft.token_id}
+                    proposeeContractAddress={nft.contract_address}
+                    proposeeTokenId={nft.token_id}
                   />
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </div>
-      <div style={{ marginTop: "100px" }}>
-        <h2>NFTs available for swap</h2>
-        <div className={styles.description}>
-          <div className={styles.grid}>
-            {availableNFTs.map((nft) => {
-              return (
-                <div
-                  key={nft.token_id}
-                  className={styles.card}
-                  style={{ marginTop: "20px" }}
-                >
-                  <h2 style={{ textAlign: "center" }}>{nft.name}</h2>
-
-                  <a href={nft.image} target="_blank" rel="noopener noreferrer">
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img src={nft.image} alt={nft.description} />
-                  </a>
-                  <div style={{ marginTop: "20px", width: "100%" }}>
-                    <span style={{ paddingBottom: "10px" }}>
-                      {nft.description}
-                    </span>
-                    <span
-                      onClick={() => {
-                        navigator.clipboard.writeText(nft.contract_address);
-                        console.log(
-                          "copied contract address ",
-                          nft.contract_address
-                        );
-                      }}
-                      style={{ paddingBottom: "10px", cursor: "pointer" }}
-                    >
-                      Contract: {displayPartialAddress(nft.contract_address)}
-                    </span>
-                    <span
-                      onClick={() => {
-                        navigator.clipboard.writeText(nft.token_id);
-                        console.log("copied token id ", nft.token_id);
-                      }}
-                      style={{ cursor: "pointer" }}
-                    >
-                      Token ID: {nft.token_id}
-                    </span>
-                  </div>
                 </div>
               );
             })}
